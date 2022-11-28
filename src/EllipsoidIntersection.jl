8e78c0bdad1f054813f84a5a7b422aeebe532eb2
JulienCalbert <julien.calbert@uclouvain.be>
2022-11-28 20:30:00 +0100

    add intersection and Plot v2

---
+++ src/EllipsoidIntersection.jl
@@ -0,0 +1,49 @@
+
+
+function intersect(elli1::Ellipsoid, elli2::Ellipsoid)
+    if  elli1.c==elli2.c
+        return true
+    elseif (elli1.c ∈ elli2) || (elli2.c ∈ elli1)
+        return true
+    else 
+        L = cholesky((elli2.P+elli2.P')/2).L #TODO remove ' when Ellipsoid constructor fixed
+        P = L\elli1.P/L';
+        c = L'*(elli1.c -elli2.c)
+        specDecomp = eigen(P)
+        lb = specDecomp.values
+        ct = specDecomp.vectors'*c
+
+        polPos(β) = -(-1-β + sum((β*lb./(1 .+ β*lb)).*(ct.^2)))
+        dpolPos(β) = -(-1 + sum((lb./(1 .+ β*lb).^2).*(ct.^2)))
+        ddpolPos(β) = -(-2*sum((lb.^2 ./(1 .+ β*lb).^3).*(ct.^2)))
+        if(norm(ct)^2-1<0)
+            return false
+        end
+        (val, _) = dbisection(polPos, dpolPos, ddpolPos, interval=[0, norm(ct)^2-1], verbose=false) #, stopIfPositive=true)
+
+        return val>=0
+    end
+end
+
+
+function get_ℓ_ast_intersect(elli1::Ellipsoid, elli2::Ellipsoid)
+    L = cholesky((elli2.P+elli2.P')/2).U #TODO remove ' when Ellipsoid constructor fixed
+    P = L'\elli1.P/L;
+    specDecomp = eigen(P)
+    lb = specDecomp.values
+    ct = specDecomp.vectors'*L*(elli1.c -elli2.c)
+
+    polPos(β) = -(-1-β + sum((β*lb./(1 .+ β*lb)).*(ct.^2)))
+    dpolPos(β) = -(-1 + sum((lb./(1 .+ β*lb).^2).*(ct.^2)))
+    ddpolPos(β) = -(-2*sum((lb.^2 ./(1 .+ β*lb).^3).*(ct.^2)))
+
+    ub = norm(ct)^2-1;#α*2;
+    #while dpolPos(ub) < 0
+    #    ub*=2
+    #end
+    (val, β) = dbisection(polPos, dpolPos, ddpolPos, interval=[0, ub], verbose=false, stopIfNegative=false)
+
+    return -val+1, β
+end
+
+
---
+++ src/Ellipsoids.jl
@@ -0,0 +1,80 @@
+module Ellipsoids
+using LinearAlgebra
+using SpecialFunctions
+using Plots
+using LazySets
+
+struct Ellipsoid{T<:Real,MT<:AbstractMatrix{T},VT<:AbstractVector{T}}
+    P::MT
+    c::VT
+    function Ellipsoid(P::MT,c::VT) where {T<:Real,MT<:AbstractMatrix{T},VT<:AbstractVector{T}}
+        P_ = (P+P')
+        if eigmin(P_) > 0
+            new{T,MT,VT}(P_./2,c)
+        else
+            error("P must be a positive definite matrix")
+        end
+
+    end
+end
+
+function Base.:*(elli::Ellipsoid, r::Real)
+    Ellipsoid(elli.P*(1/r), elli.c)
+end
+
+function Base.:*(r::Real, elli::Ellipsoid)
+    elli*r
+end
+
+function Base.:/(elli::Ellipsoid, r::Real)
+    elli*(1/r)
+end
+
+
+function Base.:∉(elli1::Ellipsoid, elli2::Ellipsoid)
+    !(elli1 ∈ elli2)
+end
+
+
+function Base.in(x::AbstractVecOrMat, elli::Ellipsoid)
+    return (x-elli.c)'elli.P*(x-elli.c) ≤ 1
+end
+
+
+function centerDistance(elli1::Ellipsoid,elli2::Ellipsoid)
+    return norm(get_center(elli1)-get_center(elli2))
+end
+
+function pointCenterDistance(elli::Ellipsoid, x)
+    return norm(get_center(elli)-x)
+end
+
+function volume(elli::Ellipsoid)
+    N = size(elli.P,1)
+    return pi^(N/2)/(gamma(N/2+1))*det(elli.P)^(-1/2)
+end
+
+function get_center(elli::Ellipsoid)
+    return elli.c
+end
+
+function get_dims(elli::Ellipsoid)
+    return length(elli.c)
+end
+
+function scale(elli::Ellipsoid, α)
+    return Ellipsoid(elli.P*(1/α),elli.c*α)
+end
+
+function plot_ellipsoid!(elli::Ellipsoid; label="")
+    E = LazySets.Ellipsoid(elli.c, inv(elli.P))
+    plot!(E, 1e-3,label=label)
+end
+
+
+include("EllipsoidInclusion.jl")
+include("EllipsoidIntersection.jl")
+
+export Ellipsoid
+
+end # module
